a4c7598ddac3b18eca93e652c92d1d43b1b73c25
ryzetech <ryzetechde@gmail.com>
2021-11-21 15:02:04 +0100

    aaaaaaaaa

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('./token.json');
 // const { prefix } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once("ready", () => {
   console.log("Ready!");
