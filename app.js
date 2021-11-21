43240479560548bade59408ac1e8ed0fba9663d0
ryzetech <ryzetechde@gmail.com>
2021-11-21 14:31:52 +0100

    embed send

--- app.js
+++ app.js
@@ -4,8 +4,38 @@ const { token } = require('./token.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once('ready', () => {
-  console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("messageCreate", message => {
+  const channel = message.channel;
+
+  channel.send({
+    "embeds": [
+      {
+        "type": "rich",
+        "title": `Lil WHÄGY - Social Media`,
+        "description": `Do you want to hear more froim **Lil WHÄGY**? Check out his other presences!`,
+        "color": 0x230633,
+        "fields": [
+          {
+            "name": `Exclusive Songs`,
+            "value": `[SoundCloud](https://soundcloud.com/user-411463626)`,
+            "inline": true,
+          },
+          {
+            "name": `Visualizers`,
+            "value": `[YouTube](https://www.youtube.com/channel/UCNWst-KiCxNnpH3pdhPOINg)`,
+            "inline": true,
+          },
+        ],
+        "footer": {
+          "text": `bot by https://ryzetech.live/ | service provided for free`,
+        },
+      },
+    ],
+  });
 });
 
 client.login(token);
