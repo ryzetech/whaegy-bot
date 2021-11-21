5e5fda44e5e7114f0d4da4782940f66616f5330a
ryzetech <ryzetechde@gmail.com>
2021-11-21 15:19:25 +0100

    randomness xdddd

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('./token.json');
 // const { prefix } = require('./config.json');
 
+let counter = 0;
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once("ready", () => {
@@ -11,12 +13,19 @@ client.once("ready", () => {
 client.on("messageCreate", message => {
   if (message.author === client.user) return;
 
+  const random = Math.floor(Math.random() * 100) + 1;
+
+  if (counter < random) {
+    counter++;
+    return;
+  }
+
   message.channel.send({
     "embeds": [
       {
         "type": "rich",
-        "title": `Mark Rose - Social Media`,
-        "description": `Do you want to hear more froim **Mark Rose**? Check out his other presences!`,
+        "title": `Mawk Rose - Social Media`,
+        "description": `Do you want to hear more froim **Mawk Rose**? Check out his other presences!`,
         "color": 0x230633,
         "fields": [
           {
