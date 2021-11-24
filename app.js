a2a9ab856e507bf5f7938c940f1efc4e1fb354ff
ryzetech <ryzetechde@gmail.com>
2021-11-24 20:56:32 +0100

    invite patch + logging

--- app.js
+++ app.js
@@ -4,10 +4,38 @@ const { token } = require('./token.json');
 
 let counter = 0;
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_INVITES] });
+
+function checkInviteValidity() {
+  // get all invites
+  // if an invite is older than 1 hour and is used, delete it
+  // log the number of invites deleted
+  let deleted = 0;
+
+  client.guilds.cache.forEach((guild) => {
+    guild.fetchInvites().then((invites) => {
+      invites.forEach((invite) => {
+        if (invite.uses > 0 && invite.createdTimestamp < Date.now() - 3600000) {
+          invite.delete();
+          deleted++;
+        }
+      });
+    });
+  });
+
+  if (deleted > 0) {
+    client.channels.cache.get("913155351912775731").send(`✅ Deleted ${deleted} invites in hourly clean!`);
+  }
+}
 
 client.once("ready", () => {
-  console.log("Ready!");
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('with ryze and mawk', { type: 'PLAYING' });
+  client.channels.cache.get("913155351912775731").send(`✅ I'm logged in and now watching!`);
+
+  checkInviteValidity();
+
+  setInterval(checkInviteValidity, 3600000);
 });
 
 client.on("messageCreate", message => {
@@ -22,7 +50,6 @@ client.on("messageCreate", message => {
   }
 
   counter = 0;
-
   message.channel.send({
     "embeds": [
       {
